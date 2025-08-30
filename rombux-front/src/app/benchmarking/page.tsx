65aec31e18090acd69a5377ad0190eda1695d0cd
Luis Muñoz <l.munoz.m@outlook.com>
2025-08-30 15:17:36 -0400

    Fix import path for SubNavBar component

--- rombux-front/src/app/benchmarking/page.tsx
+++ rombux-front/src/app/benchmarking/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SubNavBar from "@/components/SubNavBar/SubNavBar";
+import SubNavBar from "@/components/SubNavBar/SubNavbar";
 import Benchmarking from './Benchmarking';
 import BenchmarkingCards from "@/app/benchmarking/BenchmarkingCards";
 import BenchmarkingForm from './BenchmarkingForm';
